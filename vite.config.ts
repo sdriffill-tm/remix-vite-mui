248e2f87d72ec2fe8df32c57614320b72e584656
Steve Driffill <s.driffill@techmodal.com>
2024-08-19 17:07:55 +0100

    Update vite config including theme.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,45 @@
+import { extendTheme } from "@mui/material";
+import { pigment } from "@pigment-css/vite-plugin";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { red } from "@mui/material/colors";
 
 installGlobals();
 
+/** @type {import('@pigment-css/vite-plugin').PigmentOptions} */
+const pigmentConfig = {
+  theme: extendTheme({
+    colorSchemes: {
+      light: {
+        palette: {
+          primary: {
+            main: "#556cd6",
+          },
+          secondary: {
+            main: "#19857b",
+          },
+          error: {
+            main: red.A400,
+          },
+        },
+      },
+    },
+  }),
+  transformLibraries: ["@mui/material"],
+};
+
 export default defineConfig({
-  ssr: {
-    noExternal: process.env.NODE_ENV === "production" ? [/^@mui\//] : [],
+  optimizeDeps: {
+    include: [
+      "@mui/material",
+      "@pigment-css/react",
+      "@mui/material-pigment-css",
+    ],
   },
-  plugins: [remix(), tsconfigPaths()],
+  ssr: {
+    noExternal: ["@mui/**", "@pigment-css/**"],
+  },
+  plugins: [pigment(pigmentConfig), remix(), tsconfigPaths()],
 });
